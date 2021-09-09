b3842001890172c8146530cda523457f318028ad
Andrea Lin <andre93.lin@gmail.com>
2021-09-09 19:15:33 -0300

    parcial

---
+++ fdsfdsfd.js
@@ -0,0 +1,16 @@
+const appProfesionales = {
+  profesionales: profesionales,
+
+  // B
+  listarProfesionales: function () {
+    //   PRIMERO PONER UN LOOP
+    for (i = 0; i < this.profesionales.length; i++) {
+      // CREAR UNA VARIABLE Y ACCEDER A LA PROPIEDAD 'verificarHabilitado' , y poner el texto 'ok' en true y 'inactivo' en false
+      let verificarHabilitado = XXXXXX ? "ok" : "inactivo";
+      // hacer el console.log, accediendo a las propiedades correspondientes
+      console.log(
+        `id ${xxxxxxx} ${xxxxx}, ${xxxxx}, ${xxxx}, $${xxxxx}, ${xxxxxxxxxxxxxx}`
+      );
+    }
+  },
+};
